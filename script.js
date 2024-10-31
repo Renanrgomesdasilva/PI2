d8397026f091c19efe63fb87069df8c9ff4c1572
Renan <renangomessilvargs@gmail.com>
2024-10-31 19:32:38 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    window.onscroll = function(){
+        const header = document.getElementById("header");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.classList.add("rolagem");
+        } else {
+            header.classList.remove("rolagem");
+        }
+    };
